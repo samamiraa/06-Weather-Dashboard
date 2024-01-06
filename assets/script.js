b3ed8d532885b437298e993101ca08391f80a8b9
Samantha Allen <samanthaamiraallen@gmail.com>
2024-01-05 22:02:53 -0400

    added previously searched, fixed local storage

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,61 @@
 let locationSearch = $("#search-btn");
 let locationValue = $("#location-input");
 let apiKey = api_Key;
+let weatherData;
 
 locationSearch.on("click", function() {
     event.preventDefault();
     console.log(locationValue.val());
+
+    let searchCity = JSON.parse(localStorage.getItem("citySearch"));
+
+    if (!searchCity) {
+        searchCity = [];
+    };
+
+    searchCity.push(locationValue.val());
+    localStorage.setItem("citySearch", JSON.stringify(searchCity));
+
     weatherAPIFetch();
+    addPreviousCity();
+
+    locationValue.val("");
 });
+
+function addPreviousCity() {
+
+    let searchCity = JSON.parse(localStorage.getItem("citySearch"));
+
+    if (!searchCity) {
+        searchCity = [];
+    };
+
+    console.log(searchCity);
+
+    for (i = 0; i < searchCity.length; i++) {
+        let previousCity = `
+        <li><a class="dropdown-item text-color" onclick="previousWeatherFetch(event)">${searchCity[i]}</a></li>
+        `;
+    
+        $(".dropdown-menu").append(previousCity);
+    };
+   
+};
 
 function weatherAPIFetch() {
     const requestUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + locationValue.val() + "/?key=" + apiKey;
 
+    fetchWeather(requestUrl);
+};
+
+function previousWeatherFetch(event) {
+    const requestUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + $(event.target).text() + "/?key=" + apiKey;
+
+    fetchWeather(requestUrl);
+};
+
+
+function fetchWeather(requestUrl) {
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
@@ -18,6 +63,11 @@ function weatherAPIFetch() {
         .then(function (data) {
             console.log(data);
         });
+
+        let weatherIcon = $(".weather-icon");
+        let dates = $(".date");
+
+        console.log()
 };
 
-
+addPreviousCity();
